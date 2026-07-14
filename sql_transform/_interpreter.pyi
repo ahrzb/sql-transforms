4a8d4538fc064f0b768adf790e987564eca6d940
AmirHossein Roozbahani <ahrzb5@gmail.com>
2026-07-14 19:19:47 +0200

    docs: update _interpreter.pyi for typed InferFn

--- sql_transform/_interpreter.pyi
+++ sql_transform/_interpreter.pyi
@@ -1,12 +1,15 @@
 from typing import Any
 
+from pydantic import BaseModel
+
 class InferFn:
+    output_model: type[BaseModel]
+
     def __init__(
         self,
         sql: str,
-        row_tables: list[str],
+        row_tables: dict[str, type[BaseModel]],
         static_tables: dict[str, Any],
+        output_model: type[BaseModel] | None = None,
     ) -> None: ...
-    def infer(
-        self, tables: dict[str, list[dict[str, Any]]]
-    ) -> list[dict[str, Any]]: ...
+    def infer(self, tables: dict[str, list[BaseModel]]) -> list[BaseModel]: ...
